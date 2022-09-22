90b26ce73c9740b27b5560868d97f58c1b2471c4
swastikpatro <patroswastik.jsdev@gmail.com>
2022-09-22 22:28:06 +0530

    refractor: selected all elements & made luckyBtn

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('Hare Krishna');
+
+const birthInput = document.querySelector('#date-input');
+const numberInput = document.querySelector('#lucky-input');
+const btnContainer = document.querySelector('.btn-container');
+const output = document.querySelector('.output-section');
+const alertText = document.querySelector('.alert');
+
+// lucky-btn
+const luckyBtn = document.createElement('button');
+luckyBtn.classList.add('btn');
+luckyBtn.setAttribute('data-btn', 'show');
+luckyBtn.innerText = 'All Lucky Nums';
